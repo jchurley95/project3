8410225862b91275f6fea179aa183e4c7b2b3696
jchurley95 <jchurley95@gmail.com>
2017-08-15 23:59:23 -0400

    Created Schema

---
+++ db/schema.js
@@ -0,0 +1,66 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+// Use native promises
+mongoose.Promise = global.Promise;
+
+const postSchema = {
+    content: String,
+    created_at: Date,
+    updated_at: Date
+}
+
+const projectSchema = new Schema({
+    name: String,
+    imageURL: String,
+    projectCost: Number,
+    totalStockBoardNeededThisProject: Number,
+    created_at: Date,
+    updated_at: Date
+});
+
+const userSchema = new Schema({
+    firstName: String,
+    lastName: String,
+    userName: String,
+    company: String,
+    posts: [postSchema],
+    projects: [projectSchema],
+    created_at: Date,
+    updated_at: Date
+});
+
+postSchema.pre('save', function(next){
+  now = new Date();
+  this.updated_at = now;
+  if ( !this.created_at ) {
+    this.created_at = now;
+  }
+  next();
+});
+projectSchema.pre('save', function(next){
+  now = new Date();
+  this.updated_at = now;
+  if ( !this.created_at ) {
+    this.created_at = now;
+  }
+  next();
+});
+userSchema.pre('save', function(next){
+  now = new Date();
+  this.updated_at = now;
+  if ( !this.created_at ) {
+    this.created_at = now;
+  }
+  next();
+});
+
+const Post = mongoose.model('Post', postSchema);
+const Project = mongoose.model('Project', projectSchema);
+const User = mongoose.model('User', userSchema);
+
+module.exports = {
+    Post: Post,
+    Project: Project,
+    User: User
+};
