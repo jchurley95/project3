4a8efa583b9f8edd162dae825b773befece7a6ad
jchurley95 <jchurley95@gmail.com>
2017-09-05 00:33:38 -0400

    Updated form css, refactored with fewer classes for universal appearance

--- client/src/components/AddPost.js
+++ client/src/components/AddPost.js
@@ -30,14 +30,16 @@ class AddPost extends Component {
 
     render() {
         return (
-            <div className="New-Post-Page-Container">
+            <div className="New-Container">
+                <br />
                 <div>
                     <form onSubmit={this.props.addNewPost}>
                     <fieldset>
-                        <legend><h1>New Post</h1></legend>
+                        <legend><h1>Submit A New Post</h1></legend>
+                        <br />
                         <div><input name="Post Title" type="text" placeholder="Post Title" onChange={this._handleNewProductChange}/></div>
                         <br/>
-                        <div><input name="Post Content" type="text" placeholder="Content" onChange={this._handleNewProductChange}/></div>
+                        <div><input className="post-content" name="Post Content" type="text" placeholder="Content" onChange={this._handleNewProductChange}/></div>
                         <br/>
                         <div><input className="submit" type="submit" value="Create New Post" onSubmit={this.props.addNewPost}/></div>
                     </fieldset>
--- client/src/components/AddProject.js
+++ client/src/components/AddProject.js
@@ -30,21 +30,34 @@ class AddProject extends Component {
 
     render() {
         return (
-            <div>
-              <br />
-              <br />
+            <div className="New-Container">
+                <br />
                 <form onSubmit={this.props.addNewProject}>
                   <fieldset>
-                    <legend><h1>Build a New Project</h1></legend>
+                    <legend><h1>Build A New Project</h1></legend>
+                    <br />
+                    <div><label>Project Name</label></div>
                     <div><input name="Project Name" type="text" placeholder="Project Name" onChange={this._handleNewProductChange}/></div>
                     <br/>
+                    <div><label>Project Image URL</label></div>
                     <div><input name="Image URL" type="text" placeholder="Image URL"/></div>
                     <br/>
+                    <div><label>Project Piece Lengths List</label></div>
                     <div><input name="Piece List" type="text" placeholder="Piece Length List" onChange={this._handleNewProductChange}/></div>
                     <br/>
-                    <div><input type="submit" value="Create New Product" onSubmit={this.props.addNewProject}/></div>
+                    <div><input className="submit" type="submit" value="Create New Product" onSubmit={this.props.addNewProject}/></div>
                   </fieldset>
                 </form>
+                <div>
+                    <Link to={`/posts/new`}>
+                        Leave A Post
+                    </Link>
+                </div>
+                <div>
+                    <Link to={`/`}>
+                        Go Home
+                    </Link>
+                </div>
             </div>
         );
 
